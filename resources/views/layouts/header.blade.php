b5167ef4e6aa513238878be03b516465b7180aed
dima887 <58649137+dima887@users.noreply.github.com>
2021-08-03 14:32:46 +0300

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,19 +1,22 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-info">
-    <div class="container-fluid">
-        <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-link" href="{{route('home')}}">Просмотр объявлений</a>
-                @auth()
-                    <a class="nav-link" href="{{route('info.parser')}}">Парсер</a>
-                @endauth
-                @guest()
-                    <a class="nav-link" href="{{route('login')}}">Войти</a>
-                    <a class="nav-link" href="{{route('register')}}">Регистрация</a>
-                @endguest
-                @auth()
-                    <a class="nav-link" href="{{route('logout')}}">Выйти</a>
-                @endauth
-            </div>
-        </div>
+  <div class="container">
+    <a class="navbar-brand" href="{{route('home')}}">Просмотр объявлений</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+         @auth()
+            <a class="nav-link" href="{{route('info.parser')}}">Парсер</a>
+        @endauth
+        @guest()
+            <a class="nav-link" href="{{route('login')}}">Войти</a>
+            <a class="nav-link" href="{{route('register')}}">Регистрация</a>
+        @endguest
+        @auth()
+            <a class="nav-link" href="{{route('logout')}}">Выйти</a>
+        @endauth
+      </div>
     </div>
+  </div>
 </nav>
